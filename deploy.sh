2b365cabe88a14dd57ed26970748d597725437fb
edwardscamera <42257904+edscamera@users.noreply.github.com>
2024-08-17 16:48:57 -0400

    Remove test

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-# Build
-rm -rf dist/
-npm install
-npx ng build
-
-git checkout --orphan gh-pages
-git rm -rf .
-cp -r dist/lastfm-weeder/browser/* .
-git add ./*.js ./*.css ./*.ico ./*.html ./*.json
